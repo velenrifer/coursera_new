80b0160290c728487ab123f67df18f82ee421611
velenrifer <lirenxi117@hotmail.com>
2019-10-28 08:23:58 +0800

    Practica 1 p5js

--- Maqueta de pruebas p5js/sketch.js
+++ Maqueta de pruebas p5js/sketch.js
@@ -1,6 +1,9 @@
 let x = 0;
 let y = 0;
 let easing = 0.05;
+let angle = 0.0;
+let r = 200;
+
 
 function setup()
 {
@@ -10,12 +13,21 @@ function setup()
 
 function draw()
 {
+    stroke(255-angle);
+let x1= width/2 + cos(angle)*(r-angle);
+let y1= height/2 + sin(angle)*(r-angle);
+line(width/2, height/2, x1, y1);
+angle += 0.01; 
+
     arc(130, 70,120, 60, radians(40), TWO_PI);
     bezier(50, 200, 300, 500, 90, 10, 220, 90);
+
     
 let targetX = mouseX;
 let targetY = mouseY;
     x += (targetX -x)* easing;
     y += (targetY -y)* easing;
     ellipse(x ,y, 30, 30);
+
+
 }
